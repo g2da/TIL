14dffd03321e983c9ad845a6a11f2b37c0d0cb34
g2 Hong <80965583+g2da@users.noreply.github.com>
2024-10-20 22:00:29 +0900

    [level 1] Title: 소수 만들기, Time: 10.77 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,25 @@
+// 소수 판별 함수
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) {
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+}
